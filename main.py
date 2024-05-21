943a22e170c7766797a52818366934bafba59f7d
ahismaily <ahismailg@gmail.com>
2024-05-21 09:28:26 +0100

    main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+fruits = ["apples","bananas"]
+liquid = ["milk","water"]
+basket = [fruits,liquid]
+print(basket)
+print("press Enter to change the content...\n")
+fruits.insert(0,"oranges")
+fruits.append("kiwis")
+liquid.insert(0,"coffee")
+liquid.remove("water")
+liquid.append("tea")
+number = [1,2,3]
+basket.append(number)
+print (basket)
